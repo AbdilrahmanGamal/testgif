aabc8a93a3fd023658fb31acb5fdd1cd7c692949
Abdilrahman <abdilrahman50@gmail.com>
2019-12-30 18:48:18 +0200

    Initial Commit

---
+++ testgif/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testgif
+//
+//  Created by Abdilrahman on 12/30/19.
+//  Copyright © 2019 Abdilrahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
